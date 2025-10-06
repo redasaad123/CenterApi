682973c3148beed13182a3abbc99b10dcd82d266
Reda Saad <redasaad010260@gmail.com>
2025-10-06 17:05:22 +0300

    commit

---
+++ Terraform/main.tf
@@ -0,0 +1,132 @@
+provider "azurerm" {
+  features {}
+
+    subscription_id = var.config.subscription_id
+    tenant_id       = var.config.tenant_id
+    client_id       = var.config.client_id
+    client_secret   = var.config.client_secret
+    
+}
+
+data "azurerm_resource_group" "rg" {
+    name = var.rg.name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "CenterVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "CenterSubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+    name                = "CenterNSG"
+    location            = data.azurerm_resource_group.rg.location
+    resource_group_name = data.azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "allow_ssh"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "allow_http"
+        priority                   = 101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "allow_https"
+        priority                   = 102
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"          
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+    subnet_id                 = azurerm_subnet.subnet.id
+    network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "CenterNIC"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+  
+}
+
+resource "azurerm_public_ip" "public_ip" {
+    name                = "CenterPublicIP"
+    location            = data.azurerm_resource_group.rg.location
+    resource_group_name = data.azurerm_resource_group.rg.name
+    allocation_method   = "Static"
+
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "CenterVM"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  
+}
+
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+
