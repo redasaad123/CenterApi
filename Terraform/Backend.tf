8fc303bc242ea8d39c0187216982f1f4409b4de0
Reda Saad <redasaad010260@gmail.com>
2025-10-07 02:03:06 +0300

    commit

--- Terraform/Backend.tf
+++ Terraform/Backend.tf
@@ -13,11 +13,6 @@ terraform {
     storage_account_name  = "redastorageaccnt1234"
     container_name        = "content"
     key                   = "terraform.tfstate"
-
-    subscription_id       = var.subscription_id
-    tenant_id             = var.tenant_id
-    client_id             = var.client_id
-    client_secret         = var.client_secret
   }
 }
 
