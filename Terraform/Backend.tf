863f15756dce0ea0741ef021702b50563fa6d89f
Reda Saad <redasaad010260@gmail.com>
2025-10-06 19:37:59 +0300

    commit

---
+++ Terraform/Backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "Reda"
+        storage_account_name  = "redastorageaccnt1234"
+        container_name        = "content"
+        key                   = "./terraform.tfstate"
+    }
+}
